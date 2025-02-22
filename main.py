8673120e33044216acaabb943a6dc4fd768d9133
Ltk-Mxz <155979116+Ltk-Mxz@users.noreply.github.com>
2025-02-22 17:27:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+from openai import OpenAI
+
+# Model to use
+model = "gpt-3.5-turbo"
+
+# Client
+client = OpenAI(api_key=os.environ["OPENAI"])
+
+# Conversation
+conversation =[
+    {
+        "role": "system",
+        "content":"You are a travel guide designed to provide information about landmarks that tourists should explore in Paris. You speak in a concise manner."
+    },
+    {
+        "role":"user",
+        "content":"What is the most famous landmark in Paris?"
+    },
+    {
+        "role":"assistant",
+        "content":"The most famous landmark in Paris is the Eiffel Tower."
+    },
+]
+
+# List of questions
+questions = [
+    "How far away is the Louvre from the Eiffel Tower (in driving miles)?",
+    "Where is the Arc de Triomphe?",
+    "What are the must-see artworks at the Louvre Museum?",
+]
+
+# Loop through each question to generate responses
+for question in questions:
+
+    # Format the user input into dictionary form
+    input_dict = {"role": "user", "content": question}
+    
+    # Add the user input dictionary to the conversation
+    conversation.append(input_dict)  
+
+    # Make the next API call
+    response = client.chat.completions.create(
+        model=model,
+        messages=conversation,
+        temperature=0.0,
+        max_tokens=100
+    )
+    
+    # Print the response from the model
+    resp = response.choices[0].message.content
+    print(resp)
+
+    # Convert the response into the dictionary
+    resp_dict = {"role": "assistant", "content": resp}
+    
+    # Append the response to the conversation
+    conversation.append(resp_dict)
